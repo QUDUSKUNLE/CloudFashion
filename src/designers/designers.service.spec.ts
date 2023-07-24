1e261457e731342cf0a6d5afcf145994840b374c
QUDUSKUNLE <quduskunle@gmail.com>
2023-07-24 21:05:47 +0100

    Write designers.service unit tests

--- src/designers/designers.service.spec.ts
+++ src/designers/designers.service.spec.ts
@@ -1,25 +1,114 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DesignersService } from './designers.service';
+import { createRequest } from 'node-mocks-http';
 import { PrismaService } from '../prisma/prisma.service';
-import { DesignersResolver } from './designers.resolver';
-
-import { UsersModule } from '../users/users.module';
-import { AuthModule } from '../services/auth/auth.module';
-import { RedisCacheModule } from '../services/redis-cache/redis-cache.module';
+import { MockData, MODULE } from '../mock/mock.data';
 
 describe('DesignersService', () => {
   let service: DesignersService;
+  let prisma: PrismaService;
+
+  const req = createRequest({
+    method: 'POST',
+    url: '/',
+  });
+  req.sub = MockData.SERVICES[MODULE.USER].USER;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, AuthModule, RedisCacheModule],
-      providers: [DesignersService, DesignersResolver, PrismaService],
+      providers: [DesignersService, PrismaService],
     }).compile();
 
     service = module.get<DesignersService>(DesignersService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  describe('Create method', () => {
+    it('should be able to create a new designer', async () => {
+      prisma.designers.create = jest.fn().mockReturnValue({
+        ...MockData.SERVICES[MODULE.DESIGNER].CREATE_DESIGNER,
+        UserID: req.sub.UserID,
+      });
+      prisma.users.update = jest.fn().mockReturnValue({});
+      const result = await service.Create(
+        MockData.SERVICES[MODULE.DESIGNER].CREATE_DESIGNER,
+        req,
+      );
+      expect(prisma.designers.create).toHaveBeenCalled();
+      expect(result.UserID).toEqual(req.sub.UserID);
+    });
+    it('should throw error when a designer already registered', () => {
+      prisma.designers.create = jest.fn().mockRejectedValue({
+        code: 'P2002',
+        message: MockData.SERVICES[MODULE.DESIGNER].ERROR_MESSAGE,
+      });
+      return service
+        .Create(MockData.SERVICES[MODULE.DESIGNER].CREATE_DESIGNER, req)
+        .catch((e) =>
+          expect(e?.message).toEqual(
+            MockData.SERVICES[MODULE.DESIGNER].ERROR_MESSAGE,
+          ),
+        );
+    });
+  });
+  describe('FindAll method', () => {
+    it('should return array of designers', async () => {
+      prisma.designers.findMany = jest.fn().mockReturnValue([]);
+      await service.FindAll(MockData.SERVICES.ARGS);
+      expect(prisma.designers.findMany).toHaveBeenCalled();
+    });
+  });
+  describe('FindOne method', () => {
+    it('should return a designer of a given ID', async () => {
+      prisma.designers.findUnique = jest.fn().mockReturnValue({
+        ...MockData.SERVICES[MODULE.DESIGNER].FIND_ONE,
+        DesignerID: '1',
+      });
+      await service.FindOne('1', req);
+      expect(prisma.designers.findUnique).toHaveBeenCalled();
+    });
+    it('should throw error when a given designer ID is not found', async () => {
+      prisma.designers.findUnique = jest.fn().mockReturnValue(null);
+      return await service
+        .FindOne('1', req)
+        .catch((e) => expect(e?.message).toEqual('Designer 1 not found.'));
+    });
+  });
+  describe('Update method', () => {
+    it('should return an updated designer', async () => {
+      prisma.designers.update = jest.fn().mockReturnValue({
+        ...MockData.SERVICES[MODULE.DESIGNER].FIND_ONE,
+        DesignerID: '1',
+      });
+      await service.Update(
+        {
+          ...MockData.SERVICES[MODULE.DESIGNER].FIND_ONE,
+          DesignerID: '1',
+        },
+        req,
+      );
+      expect(prisma.designers.update).toHaveBeenCalled();
+    });
+  });
+  describe('Remove method', () => {
+    it('should archive a designer', async () => {
+      prisma.designers.findUnique = jest.fn().mockReturnValue({
+        ...MockData.SERVICES[MODULE.DESIGNER].FIND_ONE,
+        DesignerID: '1',
+      });
+      const result = await service.Remove('1', req);
+      expect(prisma.designers.findUnique).toHaveBeenCalled();
+      expect(result).toBeDefined();
+      expect(result).toEqual(MockData.SERVICES[MODULE.DESIGNER].REMOVE_MESSAGE);
+    });
+    it('should throw error when a given designer ID is not found', async () => {
+      prisma.designers.findUnique = jest.fn().mockReturnValue(null);
+      return await service
+        .Remove('1', req)
+        .catch((e) => expect(e?.message).toEqual('Designer 1 not found.'));
+    });
   });
 });
--- src/designers/designers.service.ts
+++ src/designers/designers.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
 import * as express from 'express';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Role } from '../common/interface';
+import { FetchArgs } from '../common/address.input';
 import { CreateDesignerInput } from './dto/create-designer.input';
 import { UpdateDesignerInput } from './dto/update-designer.input';
 import { PrismaService } from '../prisma/prisma.service';
@@ -9,31 +10,41 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class DesignersService {
   constructor(private readonly prismaService: PrismaService) {}
-  async create(createDesignerInput: CreateDesignerInput, req: express.Request) {
-    await this.prismaService.users.update({
-      where: { UserID: req.sub.UserID },
-      data: { Roles: req.sub.Roles as string[] },
-    });
-    return await this.prismaService.designers.create({
-      data: {
-        UserID: req.sub.UserID,
-        DesignerName: createDesignerInput.DesignerName,
-        DesignerPhoneNumbers: createDesignerInput.DesignerPhoneNumbers,
-        DesignerAddress: {
-          StreetNo: createDesignerInput.DesignerAddress.StreetNo,
-          StreetName: createDesignerInput.DesignerAddress.StreetName,
-          City: createDesignerInput.DesignerAddress.City,
-          State: <string>(createDesignerInput.DesignerAddress.State as unknown),
+  async Create(createDesignerInput: CreateDesignerInput, req: express.Request) {
+    try {
+      const result = await this.prismaService.designers.create({
+        data: {
+          UserID: req.sub.UserID,
+          DesignerName: createDesignerInput.DesignerName,
+          DesignerPhoneNumbers: createDesignerInput.DesignerPhoneNumbers,
+          DesignerAddress: {
+            StreetNo: createDesignerInput.DesignerAddress.StreetNo,
+            StreetName: createDesignerInput.DesignerAddress.StreetName,
+            City: createDesignerInput.DesignerAddress.City,
+            State: <string>(
+              (createDesignerInput.DesignerAddress.State as unknown)
+            ),
+          },
         },
-      },
+      });
+      await this.prismaService.users.update({
+        where: { UserID: req.sub.UserID },
+        data: { Roles: req.sub.Roles as string[] },
+      });
+      return result;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async FindAll(fetchArgs: FetchArgs) {
+    return await this.prismaService.designers.findMany({
+      skip: fetchArgs.skip,
+      take: fetchArgs.take,
     });
   }
 
-  async findAll() {
-    return await this.prismaService.designers.findMany();
-  }
-
-  async findOne(DesignerID: string, req: express.Request) {
+  async FindOne(DesignerID: string, req: express.Request) {
     const designer = await this.prismaService.designers.findUnique({
       where: {
         DesignerID,
@@ -44,7 +55,7 @@ export class DesignersService {
     throw new NotFoundException(`Designer ${DesignerID} not found.`);
   }
 
-  async update(updateDesignerInput: UpdateDesignerInput, req: express.Request) {
+  async Update(updateDesignerInput: UpdateDesignerInput, req: express.Request) {
     return await this.prismaService.designers.update({
       where: {
         DesignerID: updateDesignerInput.DesignerID,
@@ -56,7 +67,7 @@ export class DesignersService {
     });
   }
 
-  async remove(DesignerID: string, req: express.Request) {
+  async Remove(DesignerID: string, req: express.Request) {
     const designer = await this.prismaService.designers.findUnique({
       where: { DesignerID, UserID: req.sub.UserID },
     });
