d83b31f9881d9879d41b2245395e57e83135bbb5
QUDUSKUNLE <quduskunle@gmail.com>
2023-07-31 17:05:16 +0100

    Abstract Validation Constructor Class

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,28 +1,25 @@
 import { Address, FetchArguments } from './address.input';
-import { FetchCustomersArgument } from './customers';
-import { FetchDesignersArgument } from './designers';
 import { ItemStatus, Role, State } from './interface';
-import { MoongooseIDValidator } from './mongoose.id.validation';
+import {
+  isObjectIDValid,
+  ValidationConstructor,
+  MoongooseIDValidator,
+} from './mongoose.id.validation';
 import { PrismaService } from './prisma/prisma.service';
-import { FetchProductsArgument } from './products';
 import { RolesGuard } from './roles.guard';
 import { GraphRequest, Roles } from './user.decorator';
-import { FetchUsersArgument } from './users';
 
 export {
   Address,
   FetchArguments,
-  FetchCustomersArgument,
-  FetchDesignersArgument,
-  FetchProductsArgument,
-  FetchUsersArgument,
   GraphRequest,
   ItemStatus,
-  MoongooseIDValidator,
   PrismaService,
   Role,
   Roles,
+  MoongooseIDValidator,
+  ValidationConstructor,
   RolesGuard,
-  State
+  State,
+  isObjectIDValid,
 };
-
